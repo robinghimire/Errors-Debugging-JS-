809a95a2d3eab42c7c083c36cbe7a8c49e898879
robinghimire <rghimire@icstars.org>
2026-02-23 14:56:39 -0600

    Debugging and adding function with check error logs:

--- Errors & Debugging/index.js
+++ Errors & Debugging/index.js
@@ -2,14 +2,30 @@
 
 function calculateTotal(numbers) {
  let total = 0;
- for (let i = 0; i <= numbers.length; i++) { // BUG: <= causes undefined
+ for (let i = 0; i < numbers.length; i++) { // BUG: <= causes undefined
    total += numbers[i];
  }
  return total;
 }
 
-const data1 = [1, 2, 3];
+const data1 = [1,2, 3];
 const data2 = [4, 5, 6];
 
-console.log(calculateTotal(data1));
-console.log(calculateTotal(data2));
+function findMax(numbers) {
+  let max = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > max) max = numbers[i];
+  }
+  return max;
+}
+
+const number3 = [1,2,3,9,10];
+
+
+try {
+  console.log(calculateTotal([1, "2", null, "fix me"])); 
+  console.log(findMax([]));                          
+  console.log(calculateTotal("Sorry- fix me here as well"));              
+} catch (e) {
+  console.error(e.message);
+}
